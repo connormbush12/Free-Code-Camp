7ac6e30ec8b73a19e57b323c5e6aadf40550bbe8
Connor Bush <connormbush12@gmail.com>
2020-11-23 16:43:28 -0600

    Basic Javascript - Lessons 30 - 54

---
+++ 02JavaScriptAlgorithms/01BasicJavaScript/38nestedArray.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+var myArray = [['cool dogs', 5], ['cool cats', 12]];
---
+++ 02JavaScriptAlgorithms/01BasicJavaScript/46groceryList.js
@@ -0,0 +1,7 @@
+var myList = [
+    ['milk', 2],
+    ['cheese', 12],
+    ['oreos', 1],
+    ['coffee', 11],
+    ['milk again', 2000000]
+];
