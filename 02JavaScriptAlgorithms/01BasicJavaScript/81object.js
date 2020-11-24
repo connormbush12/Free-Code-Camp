83aee66c856adf7f5a196b930e901bbb41d9e39b
Connor Bush <connormbush12@gmail.com>
2020-11-24 15:57:50 -0600

    Basic JavaScript Lessons 74-86

---
+++ 02JavaScriptAlgorithms/01BasicJavaScript/81object.js
@@ -0,0 +1,10 @@
+var myDog = {
+    // Only change code below this line
+        name : 'slimy, swimming dog',
+        legs : 8,
+        tails : 0,
+        friends : ['crab', 'fish', 'shark', 'algae']
+    
+    // Only change code above this line
+    };
+    
---
+++ 02JavaScriptAlgorithms/01BasicJavaScript/86addObjectProp.js
@@ -0,0 +1,11 @@
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "quack"
+  
+  // Only change code below this line
+  
