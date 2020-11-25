41f3753d7f9b3aab09145b6531dc9654b797b2ec
Connor Bush <connormbush12@gmail.com>
2020-11-25 14:56:04 -0600

    Basic JavaScript Lessons 87 - 102

---
+++ 02JavaScriptAlgorithms/01BasicJavaScript/90addRecord.js
@@ -0,0 +1,26 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    // Add a record here
+    {
+      artist : "Kanye West",
+      title : "My Beautiful, Dark, Twisted Fantasy",
+      release_year : 2010,
+      formats : [
+        "CD",
+        "Leaks",
+        "LP",
+        'Vinyl'
+      ]
+    }
+  ];
+  
