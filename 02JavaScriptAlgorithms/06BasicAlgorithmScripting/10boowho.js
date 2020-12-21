954568267489b8118928f79787f967209c12d246
Connor Bush <connormbush12@gmail.com>
2020-12-21 15:25:01 -0600

    Two free code camp problems

---
+++ 02JavaScriptAlgorithms/06BasicAlgorithmScripting/10boowho.js
@@ -0,0 +1,6 @@
+//Check if a value is classified as a boolean primitive. Return true or false.
+function booWho(bool) {
+    return typeof bool === 'boolean' ? true : false
+  }
+  
+  booWho(null);
