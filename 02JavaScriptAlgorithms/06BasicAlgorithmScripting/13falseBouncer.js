9d6fcdb5456a043ae175730ed067b2cae86fa437
Connor Bush <connormbush12@gmail.com>
2021-01-01 15:29:21 -0600

    Remove falses from an array

---
+++ 02JavaScriptAlgorithms/06BasicAlgorithmScripting/13falseBouncer.js
@@ -0,0 +1,9 @@
+// Remove all falses from array
+function bouncer(arr) {
+    const trues = arr.filter((el) => {
+      return el 
+    })
+    return trues;
+  }
+  
+  bouncer([7, "ate", "", false, 9]);
